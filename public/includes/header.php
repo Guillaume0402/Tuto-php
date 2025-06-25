6844aa347ede6a1f82bfcc5db62bcc454e12e2fa
Guillaume0402 <168349909+Guillaume0402@users.noreply.github.com>
2025-06-25 13:12:15 +0200

    maj des chemins CSS

--- public/includes/header.php
+++ public/includes/header.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tuto PHP</title>
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/main.css">
+    <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 
 <body<?= isset($moduleClass) ? ' class="' . $moduleClass . '"' : '' ?>>
